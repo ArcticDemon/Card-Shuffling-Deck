841037a1b717ab13e88def99836ec5b766c77223
ArcticDemon <65990674+ArcticDemon@users.noreply.github.com>
2020-06-10 15:00:10 +1200

    added dict of cards

--- AS Testing/Card deck testing.py
+++ AS Testing/Card deck testing.py
@@ -2,30 +2,30 @@ import random
 from random import shuffle 
 
 
-stack_1 = []
-stack_2 = []
-reserve_stack = []
+#stack_1 = []
+#stack_2 = []
+#reserve_stack = []
 
 
 
 
 
-def button():
-    card_deck = ["-1H","-1D","1C","1S","9S","-9H","13S","12D"]
-    print(card_deck)
-    user_input = input("Press Enter to shuffle cards\nOr [q] to quit: ")
+#def button():
+    #card_deck = ["-1H","-1D","1C","1S","9S","-9H","13S","12D"]
+    #print(card_deck)
+    #user_input = input("Press Enter to shuffle cards\nOr [q] to quit: ")
     
-    if len(user_input)==0:
+    #if len(user_input)==0:
         
     
-        random.shuffle(card_deck)
+        #random.shuffle(card_deck)
     
-    else: 
-        quit()
+    #else: 
+        #quit()
     
-    stacker(card_deck)
+    #stacker(card_deck)
 
-    #colour_sort(card_deck)
+    ##colour_sort(card_deck)
    
     
     
@@ -33,45 +33,45 @@ def button():
     
 
 
-def colour_sort(deck):
-    for x in range(len(deck)):
+#def colour_sort(deck):
+    #for x in range(len(deck)):
         
-        y = deck[x]
-        colour_check = (y[0])
-        if colour_check == "-":
-            print("red",y)
+        #y = deck[x]
+        #colour_check = (y[0])
+        #if colour_check == "-":
+            #print("red",y)
         
-        else:
-            print("black",y)
+        #else:
+            #print("black",y)
     
-    print("----------------------------")
+    #print("----------------------------")
     
-    button()
+    #button()
 
 
 
-def stacker(deck):
-    for i in range(len(deck)):
-        if i == 0 or i == 1:
-            reserve_stack.append(deck[i])        
+#def stacker(deck):
+    #for i in range(len(deck)):
+        #if i == 0 or i == 1:
+            #reserve_stack.append(deck[i])        
         
-        elif i%2==0:
-            stack_1.append(deck[i])
-        else: 
-            stack_2.append(deck[i])
+        #elif i%2==0:
+            #stack_1.append(deck[i])
+        #else: 
+            #stack_2.append(deck[i])
         
-    print(stack_1,"\n",stack_2, "\n", reserve_stack)    
+    #print(stack_1,"\n",stack_2, "\n", reserve_stack)    
     
 
 
 
-def quit():
-    print("Quitting")
-    raise SystemExit
+#def quit():
+    #print("Quitting")
+    #raise SystemExit
 
 
 
-button()
+#button()
 
 #card_deck = ["-1H","-1D","1C","1S","9S","-9H","13S","12D"]
 #stack_1 = []
@@ -89,4 +89,20 @@ button()
         #stack_2.append(card_deck[i])
     
 #print(stack_1, "\n", stack_2, "\n", reserve_stack)
+
+#card_deck = ["-1H","-1D","1C","1S","9S","-9H","13S","12D"]
+
+#for i in range(len(card_deck)):
+    #z = (card_deck[i])
+    #for char in z: 
+         #z[-1]
+        #print(z)
+
+x = {"red":[-1,-2,-3,-4,-5], "black":[1,2,3,4,5]}
+cards = []
+for key in x: 
+    cards = x[key]
+    print(key, cards)
     
+    for i in cards: 
+        print(i)
